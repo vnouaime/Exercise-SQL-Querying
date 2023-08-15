f89fb855e2fbbf2f95f6fb0d8ea1a04b8104ae7b
Vera Nouaime <veranouaime@Veras-MacBook-Air.local>
2023-08-14 22:37:01 -0400

    Completed SQL Querying Exercise

---
+++ queries_products.sql
@@ -0,0 +1,54 @@
+-- Comments in SQL Start with dash-dash --
+
+-- Query 1 --
+INSERT INTO products (name, price, can_be_returned)
+VALUES('chair', 44.00, false);
+
+-- Query 2 --
+INSERT INTO products (name, price, can_be_returned)
+VALUES('stool', 25.99, true);
+
+-- Query 3 --
+INSERT INTO products (name, price, can_be_returned)
+VALUES('table', 124.00, false);
+
+-- Query 4 --
+SELECT * FROM products;
+
+-- Query 5 --
+SELECT name FROM products;
+
+-- Query 6 --
+SELECT name, price FROM products;
+
+-- Query 7 --
+INSERT INTO products ( name, price, can_be_returned)
+VALUES('couch', 699.00, false);
+
+-- Query 8 --
+SELECT name FROM products
+WHERE can_be_returned = false;
+
+-- Query 9 --
+SELECT name FROM products
+WHERE price < 44.00;
+
+-- Query 10 --
+SELECT name FROM products
+WHERE price >= 25.99 AND price <= 99.00;
+
+-- Query 11 --
+UPDATE products
+SET price = price - 20;
+
+-- Query 12 --
+DELETE FROM products WHERE price < 25;
+
+-- Query 13 --
+UPDATE products
+SET price = price + 20;
+
+-- Query 14 --
+UPDATE products
+SET can_be_returned = true;
+
